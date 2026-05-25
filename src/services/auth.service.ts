9348ed61892fe8c7e68d432cf721ba3a7bf58e2d
Varad163 <varadgholap163@gmail.com>
2026-05-25 14:35:39 +0530

    auth config files

---
+++ src/services/auth.service.ts
@@ -0,0 +1,28 @@
+import api from "@/lib/axios";
+
+import {
+  LoginRequest,
+  SignupRequest,
+} from "@/types/auth.types";
+
+export const loginUser = async (
+  data: LoginRequest
+) => {
+  const response = await api.post(
+    "/auth/login",
+    data
+  );
+
+  return response.data;
+};
+
+export const signupUser = async (
+  data: SignupRequest
+) => {
+  const response = await api.post(
+    "/auth/signup",
+    data
+  );
+
+  return response.data;
+};
---
+++ src/store/auth.store.ts
@@ -0,0 +1,51 @@
+import { create } from "zustand";
+
+interface AuthState {
+  token: string | null;
+  role: string | null;
+
+  setAuth: (
+    token: string,
+    role: string
+  ) => void;
+
+  logout: () => void;
+}
+
+export const useAuthStore =
+  create<AuthState>((set) => ({
+    token: null,
+    role: null,
+
+    setAuth: (token, role) => {
+      localStorage.setItem(
+        "token",
+        token
+      );
+
+      localStorage.setItem(
+        "role",
+        role
+      );
+
+      set({
+        token,
+        role,
+      });
+    },
+
+    logout: () => {
+      localStorage.removeItem(
+        "token"
+      );
+
+      localStorage.removeItem(
+        "role"
+      );
+
+      set({
+        token: null,
+        role: null,
+      });
+    },
+  }));
